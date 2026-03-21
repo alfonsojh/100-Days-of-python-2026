647d0e5867b5ca46ed0b1bbd497482c1f871bfc4
Alfonso Javier <alfonso_javier@nube.unadmexico.mx>
2026-03-20 19:14:35 -0700

    adding first day task

---
+++ Day 1  bandNameGenerator/bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("Whats the city name where you grew up?")
+animal = input("Whats the animal name?")
+print("The band name will be the: " + animal + "s from " + city )
